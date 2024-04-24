ad09bd007ba6d442bd9f054ec499ff13b380d1c0
9rato <moshood.ajose6@gmail.com>
2024-04-24 12:25:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+// Define a variable to hold the desired index page name
+$desired_index = "public/index1.php"; // Change this to the desired index page
+
+// Include the desired index page based on the value of $desired_index
+include($desired_index);
+?>
---
+++ public/index1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<
+<body>
+    <!--header-->
+    <div class="header-main-bg">
+        <!--logo and list-->
+        <img src="" alt="">
+        <h1>ShopNow</h1>
+        <div class="logo-list">
+        <a href="#">Discover</a>
+        <a href="#">Product</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact Us</a>
+        </div>
